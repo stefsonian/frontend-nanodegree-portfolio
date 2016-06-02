68421b4083daa9dd5aa65a4ab4d1fc760b146d3d
Stefan Stefansen <mail@stefanstefansen.com>
2016-06-02 15:41:33 +0200

    fix html, css, images. v1.0 - ready for github upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,60 @@
 var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    imageResize = require('gulp-image-resize'),
     webserver = require('gulp-webserver');
+
 
 gulp.task('minifyCss', function() {
     gulp.src('dev/css/*.css')
         .pipe(cleanCSS())
         .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('resizeImagesXLarge', function () {
+    gulp.src('dev/images/sunset.jpg')
+        .pipe(imageResize({
+            width: 2400
+        }))
+        .pipe(rename(function(path) {path.basename += "-xlarge";}))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('resizeImagesLarge', function () {
+    gulp.src('dev/images/sunset.jpg')
+        .pipe(imageResize({
+            width: 1200
+        }))
+        .pipe(rename(function(path) {path.basename += "-large";}))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('resizeImagesMedium', function () {
+    gulp.src('dev/images/*.jpg')
+        .pipe(imageResize({
+            width: 768
+        }))
+        .pipe(rename(function(path) {path.basename += "-medium";}))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('resizeImagesSmall', function () {
+    gulp.src('dev/images/*.jpg')
+        .pipe(imageResize({
+            width: 480
+        }))
+        .pipe(rename(function(path) {path.basename += "-small";}))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('transferCss', function() {
+    gulp.src('css/styles.css')
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('transferSvg', function() {
+    gulp.src('dev/images/cape.svg')
+        .pipe(gulp.dest('dist/images/'))
 });
 
 gulp.task('webserver', function() {
@@ -24,4 +73,4 @@ gulp.task('watch', function(){
 });
 
 
-gulp.task('default', ['minifyCss', 'webserver', 'watch']);
+gulp.task('default', ['transferCss', 'transferSvg', 'webserver', 'watch']);
